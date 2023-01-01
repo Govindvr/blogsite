5c789827b283a49aab2c0acfa2c21576cd14bda1
Govind V R <govindvr02@gmail.com>
2023-01-02 03:28:21 +0530

    add: comment section

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name = 'blog-home'),
     path('createpost/',views.createPost, name = 'blog-create'),
-    path('blog/<int:id>',views.blog, name = 'blog-details'),
+    path('blog/<int:id>',views.viewBlog, name = 'blog-details'),
+    path('blog/addComment/<int:id>',views.addComment, name = 'add-comment'),
+
 
 ]
