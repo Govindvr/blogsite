004f605fb89b7872e055d625b021393177ba87c0
alengeorge-26 <alengeorge261@gmail.com>
2023-01-08 11:13:56 +0530

    proflie update

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Userdetails(models.Model):
+    usernm=models.CharField(max_length=200,null=False,primary_key = True)
+    name=models.CharField(max_length=200)
+    description=models.CharField(max_length=200)
+    mail=models.CharField(max_length=200)
+    location=models.CharField(max_length=200)
--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+import user
+from .models import Userdetails
 
-# Create your views here.
+
 def indexView(request):
     return render(request,'index.html')
 
@@ -15,9 +18,36 @@ def registerView(request):
         form=UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login_url') 
+            username = form.cleaned_data['username']
+            new_user=Userdetails(usernm=username,name=' ',description=' ',mail=' ',location=' ')
+            new_user.save()
+            return redirect('login_url')
+    
     else:
         form=UserCreationForm()
-    return render(request,'registration/register.html',{'form':form})
+        return render(request,'registration/register.html',{'form':form})
 
  
+def profileView(request):
+    if request.user.is_authenticated:
+        username=request.user.username
+        curruser = Userdetails.objects.get(usernm=username)
+        return render(request,'profile.html',{'curruser':curruser})
+
+    else:
+        return redirect('login_url')
+
+def dataView(request):
+    usernm=request.user.username
+    name=request.POST['name']
+    description=request.POST['description'] 
+    mail=request.POST['mail']
+    location=request.POST['location']
+
+    new_user=Userdetails(usernm=usernm,name=name,description=description,mail=mail,location=location)
+    new_user.save()
+
+    return render(request,'dashboard.html')
+
+def updateView(request):
+    return render(request,'data.html')
